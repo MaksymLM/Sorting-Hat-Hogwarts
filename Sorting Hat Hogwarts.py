cfb27ca909c06b492bd95979caec5b20c28fb706
MaksymLM <lehkobytmaksym@gmail.com>
2026-01-14 19:32:39 +0100

    Add files via upload

---
+++ Sorting Hat Hogwarts.py
@@ -0,0 +1,65 @@
+Gryffindor = 0
+Ravenclaw = 0
+Hufflepuff = 0
+Slytherin = 0
+
+print("Q1) Do you like Dawn or Dusk?")
+print("    1) Dawn")
+print("    2) Dusk")
+
+q1 = int(input("Enter yout answer from 1-2: "))
+
+if q1 == 1:
+  Gryffindor += 1
+  Ravenclaw += 1
+elif q1 == 2:
+  Hufflepuff += 1
+  Slytherin += 1
+else:
+  print("Wrong input.")
+
+
+print("Q2) When I’m dead, I want people to remember me as:")
+print("    1) The Good")
+print("    2) The Great")
+print("    3) The Wise")
+print("    4) The Bold")
+
+q2 = int(input("Enter yout answer from 1-4: "))
+
+if q2 == 1:
+  Hufflepuff += 2
+elif q2 == 2:
+  Slytherin += 2
+elif q2 == 3:
+  Ravenclaw += 2
+elif q2 == 4:
+  Gryffindor += 2
+else:
+  print("Wrong input.")
+
+
+print("Q3) Which kind of instrument most pleases your ear?")
+print("    1) The violin")
+print("    2) The trumpet")
+print("    3) The piano")
+print("    4) The drum")
+
+q3 = int(input("Enter yout answer from 1-4: "))
+
+if q3 == 1:
+  Slytherin += 4
+elif q3 == 2:
+  Hufflepuff += 4
+elif q3 == 3:
+  Ravenclaw += 4
+elif q3 == 4:
+  Gryffindor += 4
+else:
+  print("Wrong input.")
+
+
+print("Gryffindor:", Gryffindor)
+print("Ravenclaw", Ravenclaw)
+print('Hufflepuff', Hufflepuff)
+print('Slytherin', Slytherin)
